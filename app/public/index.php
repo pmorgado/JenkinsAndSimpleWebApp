2fda638df23f1c07aa48d21575735d43df850630
Pedro Morgado <pedro.morgado@lamudi.com>
2015-01-11 22:35:17 +0100

    remove not necessary code

--- app/public/index.php
+++ app/public/index.php
@@ -1,9 +1,5 @@
 <?php
 ini_set('display_errors', 1);
-if (!isset($_SERVER['HTTP_HOST'])) {
-    header($_SERVER['SERVER_PROTOCOL'].' 400 Bad Request');
-    exit;
-}
 define('DOMAIN', 'http://' . $_SERVER['HTTP_HOST']);
 define('APP_PATH', dirname(__FILE__) . '/../');
 require_once APP_PATH . 'Bootstrap.php';
